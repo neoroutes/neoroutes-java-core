90318ac4ed3c3ecdb51d6ff1bae51ebea2d71ddf
mrsgoldenretriever <mrsgoldenretriever@protonmail.com>
2020-01-05 09:58:32 +0330

    Added sessions key equality test.

--- src/test/java/project/neoroutes/diffieHellman/EncryptedSessionTest.java
+++ src/test/java/project/neoroutes/diffieHellman/EncryptedSessionTest.java
@@ -5,26 +5,36 @@ import java.security.InvalidKeyException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptedSessionTest {
-    private final EncryptedSession encryptedSession;
+    private final EncryptedSession encryptedSession1;
+    private final EncryptedSession encryptedSession2;
     private final String sample = "The message to encrypt";
     private String encryptedMessage;
 
     public EncryptedSessionTest() throws InvalidKeyException {
         DiffieHellman dh1 = new DiffieHellman();
         DiffieHellman dh2 = new DiffieHellman();
-        this.encryptedSession = new EncryptedSession(dh1, dh2.getPublicKey());
+        this.encryptedSession1 = new EncryptedSession(dh1, dh2.getPublicKey());
+        this.encryptedSession2 = new EncryptedSession(dh2, dh1.getPublicKey());
     }
 
     @Test
     void encrypt() {
-        this.encryptedMessage = encryptedSession.encrypt(sample);
+        this.encryptedMessage = encryptedSession1.encrypt(sample);
         assertNotEquals(encryptedMessage, sample);
+        System.out.println("Encrypted Text: "+ this.encryptedMessage);
     }
 
     @Test
     void decrypt() {
         encrypt();
-        String decrypt = encryptedSession.decrypt(encryptedMessage);
+        String decrypt = encryptedSession1.decrypt(this.encryptedMessage);
         assertEquals(decrypt, sample);
+        System.out.println("Decrypted Text: "+ decrypt);
+    }
+
+    @Test
+    void messageEqualityTest(){
+        assertEquals(encryptedSession1.encrypt(sample), encryptedSession2.encrypt(sample));
+        System.out.println("Keys are equal");
     }
 }
